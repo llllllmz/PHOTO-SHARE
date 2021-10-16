fb4e8dcedee302d4f4a1c3c7ed1073d5665c961b
Fricken_pro <Fricken_pro@atguigu.com>
2021-10-16 18:37:21 +0800

    10.15 fragment1

--- app/src/main/java/com/example/photoshare/Fragment_1.java
+++ app/src/main/java/com/example/photoshare/Fragment_1.java
@@ -1,15 +1,25 @@
 package com.example.photoshare;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 
 public class Fragment_1 extends Fragment {
+    private RecyclerView lvPicList;
+    private List<Pic> PicData;
+    private PicAdapter adapter;
+    private Context context = null;
+    private View homeview;
+    private View picview;
 
 
     public Fragment_1() {
@@ -28,4 +38,6 @@ public class Fragment_1 extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_1, container, false);
     }
+
+
 }
